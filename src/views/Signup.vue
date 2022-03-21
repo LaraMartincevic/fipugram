57cbb635524ad3dba27de38005d9b07ad3f356fa
lara martincevic <laramartincevic88@gmail.com>
2022-03-21 23:18:34 +0100

    signup

---
+++ src/views/Signup.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="about">
+    <h1>This is an signup page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md"></div>
+        <div class="col-md-7">
+          <form>
+            <div class="form-row">
+              <div class="col">
+                <label for="exampleInputFirstname">First name</label>
+                <input
+                  type="first name"
+                  class="form-control"
+                  id="exampleInputFirstname"
+                  placeholder="First name"
+                />
+              </div>
+
+              <div class="col">
+                <label for="exampleInputLastname">Last name</label>
+                <input
+                  type="last name"
+                  class="form-control"
+                  id="exampleInputLastname"
+                  placeholder="Last name"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-112">
+                <label for="exampleInputEmail">Example email</label>
+                <input
+                  type="example email"
+                  class="form-control"
+                  id="exampleInputEmail"
+                  placeholder="Example email"
+                />
+              </div>
+
+              <div class="col-112">
+                <label for="exampleInputUsername">Username</label>
+                <input
+                  type="username"
+                  class="form-control"
+                  id="exampleInputUsername"
+                  placeholder="Username"
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-112">
+                <label for="exampleInputPassword">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
